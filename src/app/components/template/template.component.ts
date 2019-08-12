5331661265039441760b206eb0228e2276afe411
Diego Guatibonza <diego.guatibonza@bmslatinoamerica.com>
2019-08-12 14:31:15 -0500

    primera fase del formulario funcionando en la parte de tamplate falta la parte data

---
+++ src/app/components/template/template.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid #dc3545;
+    }
+  `]
+})
+export class TemplateComponent implements OnInit {
+  mostrarCamposNecesarios = false;
+  usuario: object = {
+    nombre: '',
+    apellido: null,
+    email: null,
+    pais: '',
+    sexo: null,
+    acepta: false
+  };
+
+  paises = [{
+    codigo: 'KR',
+    nombre: 'Corea del sur'
+  },
+  {
+    codigo: 'ESP',
+    nombre: 'España'
+  },
+  {
+    codigo: 'COL',
+    nombre: 'Colombia'
+  }];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  validar(form: NgForm) {
+    if (!form.value.nombre || !form.value.apellido || !form.value.email) {
+      this.mostrarCamposNecesarios = true;
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('jiji enviando');
+    console.log(form);
+    console.log('value:', form.value);
+    console.log(this.usuario);
+  }
+
+
+}
