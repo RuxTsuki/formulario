375ada76d454654e586ab12c405b3a37cf4b0a7b
diego <Diegosferas@gmail.com>
2019-08-19 19:26:08 -0500

    Form por data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-data',
@@ -7,9 +10,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  usuario: object = {
+    nombrecompleto: {
+      nombre: "d",
+      apellido: "g"
+    },
+    correo: "Diegosfera@gmail.com",
+    pasatiempos: ['correr', 'eeeeee']
+  };
+  forma: FormGroup;
+
+  constructor() {
+
+
+    this.forma = new FormGroup({
+      nombrecompleto: new FormGroup({
+        nombre: new FormControl(this.usuario[`nombrecompleto`].nombre, [Validators.required, Validators.minLength(5)]),
+        apellido: new FormControl('', [Validators.required, this.noHerrera])
+      }),
+      correo: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      pasatiempos: new FormArray([
+        new FormControl('correr', Validators.required),
+        new FormControl('eeeeeee', )
+      ]),
+      password1: new FormControl('', [Validators.required]),
+      password2: new FormControl(),
+      username: new FormControl('', [Validators.required ],this.existeUsuario)
+    });
+    // carga todo de forma automatica si es exactamente la misma estrucutra
+    // this.forma.setValue(this.usuario);
+
+    this.forma.controls[`password2`].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ]);
+
+    // devuelven un observable 
+    // me suscribo e imprimo, cadavez que cambia algo muestro data
+    this.forma.controls[`username`].valueChanges.subscribe( data => {
+      console.log(data);
+    });
+
+    this.forma.controls[`username`].statusChanges.subscribe( data => {
+      console.log(data);
+    });
+
+
+  }
 
   ngOnInit() {
   }
+
+  noHerrera(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'herrera') {
+      return { noherrera: true }
+    }
+    return null;
+  }
+
+  noIgual(control: FormControl): { [s: string]: boolean } {
+    let forma: any = this;
+    if (control.value !== forma.controls[`password1`].value) {
+      return { noIgual: true }
+    }
+    return null;
+  }
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+    const promesa = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === "strider") {
+            resolve({ existe: true });
+          } else {
+            resolve(null)
+          }
+        }, 3000);
+      }
+    );
+    return promesa;
+  }
+
+
+  guardarCambios() {
+    console.log(this.forma.value)
+    console.log(this.forma)
+    console.log(this.forma.controls.nombrecompleto['controls'].nombre.valid);
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    });
+
+  }
+
+
 
 }
